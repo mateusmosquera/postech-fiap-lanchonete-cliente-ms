c4b74d7fa3ec21bba20519d52b6e402199cfd8b6
mateusmosquera <mateusmosquera@hotmail.com>
2024-05-08 23:40:34 -0300

    arrumando maintability issues

--- src/main/kotlin/br/com/fiap/lanchonete/cliente/exception/handler/ExceptionHandler.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/cliente/exception/handler/ExceptionHandler.kt
@@ -1,13 +1,11 @@
 package br.com.fiap.lanchonete.cliente.exception.handler
 
-import br.com.fiap.lanchonete.exception.BusinessException
 import br.com.fiap.lanchonete.cliente.exception.dto.ResponseErrorDto
+import br.com.fiap.lanchonete.exception.BusinessException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
